dff433252033c2775885ac0121782fa0d35ce077
joseph <pj9epkfc@students.codeinstitute.net>
2025-07-21 18:21:44 +0100

    structure: gallery page added and all linked up

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('gallery/', views.gallery, name='gallery'),
     path('merchandise/', views.merchandise, name='merchandise'),
     path('lets-play/', views.lets_play, name='lets_play'),
 ]
-
