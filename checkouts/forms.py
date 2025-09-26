62312131749ba5086763be8a8c564ca708d132cb
joseph <pj9epkfc@students.codeinstitute.net>
2025-09-26 11:33:50 +0100

    css: adjusted the user profile on all pages for better layout

--- checkouts/forms.py
+++ checkouts/forms.py
@@ -4,15 +4,20 @@ from .models import Order
 
 class CheckoutForm(forms.ModelForm):
     # Separate address fields for user input
-    house_number = forms.CharField(max_length=50, required=True, label="House Number / Name")
-    street_name = forms.CharField(max_length=100, required=True, label="Street Name")
-    city = forms.CharField(max_length=100, required=True)
-    postcode = forms.CharField(max_length=20, required=True)
-    country = forms.CharField(max_length=100, required=True, initial="United Kingdom")
+    house_number = forms.CharField(max_length=50, required=False, label="House Number / Name")
+    street_name = forms.CharField(max_length=100, required=False, label="Street Name")
+    city = forms.CharField(max_length=100, required=False)
+    postcode = forms.CharField(max_length=20, required=False)
+    country = forms.CharField(max_length=100, required=False, initial="United Kingdom")
 
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'phone']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Make phone optional too
+        self.fields['phone'].required = False
 
     def save(self, commit=True, user=None, subtotal=None, total=None):
         """
