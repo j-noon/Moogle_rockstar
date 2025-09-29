f110398c96cecf0ce2e84f3c3469b7932ce9b20a
joseph <pj9epkfc@students.codeinstitute.net>
2025-09-29 16:24:43 +0100

    moogle urls, removed local static root url

--- moogle/urls.py
+++ moogle/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('lets-play/', include('lets_play.urls')),
     path('merchandise/', include('merchandise.urls')),
     path('checkouts/', include('checkouts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
