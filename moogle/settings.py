d4d4457d8281b122d915ba9473a50b2a6970e779
joseph <pj9epkfc@students.codeinstitute.net>
2025-09-29 14:46:37 +0100

    adjusted settings.py to get static files back

--- moogle/settings.py
+++ moogle/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
 
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
 
     'core.apps.CoreConfig',
