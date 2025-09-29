e48b88f4932df644fa578effb903d77810e3929f
joseph <pj9epkfc@students.codeinstitute.net>
2025-09-29 11:51:15 +0100

    updates for heroku deployment

--- moogle/settings.py
+++ moogle/settings.py
@@ -101,7 +101,9 @@ WSGI_APPLICATION = 'moogle.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+    )
 }
 
 
