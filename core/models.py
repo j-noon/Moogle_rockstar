8f24094b04ecc7ea09c91b56469774b0f3c72948
joseph <pj9epkfc@students.codeinstitute.net>
2025-09-29 16:41:06 +0100

    Switch profile_image to CloudinaryField

--- core/models.py
+++ core/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     moogles = models.IntegerField(default=0)
-    profile_image = models.ImageField(
-        upload_to='profile_images/',
-        blank=True,
-        null=True
-    )
+    profile_image = CloudinaryField('image', blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
