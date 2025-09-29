991cce0a2481ad04cf4241cdf3105cb3c862d6b2
joseph <pj9epkfc@students.codeinstitute.net>
2025-09-29 16:34:18 +0100

    commetned out root media in settings

--- moogle/settings.py
+++ moogle/settings.py
@@ -154,8 +154,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Stripe settings
