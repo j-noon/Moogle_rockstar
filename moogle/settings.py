c13d70e7fcbd8385a5239a7bdffabc6088c29d3f
joseph <pj9epkfc@students.codeinstitute.net>
2026-01-10 19:53:05 +0000

    Rotate Django SECRET_KEY and move to environment variables

--- moogle/settings.py
+++ moogle/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=o=#-drmfejz3%0^shq&g-8pb67w^yp6xs4y1=+#5ns65n1ckq'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
